a06acb29e4229ad27e01a434acf8f7b29791ff29
Willian It4you <194798119+williamit4you@users.noreply.github.com>
2026-04-24 00:03:41 -0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  transpilePackages: [
+    "@remotion/bundler",
+    "@remotion/renderer",
+    "remotion",
+    "@remotion/studio-shared",
+    "@remotion/cli"
+  ],
   experimental: {
     outputFileTracingIncludes: {
       "/**/*": [
-        "./node_modules/@remotion/bundler/**/*",
-        "./node_modules/@remotion/renderer/**/*",
+        "./node_modules/@remotion/**/*",
         "./node_modules/remotion/**/*",
         "./node_modules/puppeteer/**/*",
+        "./node_modules/puppeteer-core/**/*",
+        "./node_modules/@remotion/studio-shared/**/*",
       ],
     },
   },
